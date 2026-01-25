a04911022c56d948a639eddacf2e946f698cdb3a
SBrookhart <sabina.brookhart@gmail.com>
2026-01-24 19:11:58 -0500

    Update blobTest.js

--- netlify/functions/blobTest.js
+++ netlify/functions/blobTest.js
@@ -1,7 +1,11 @@
 import { getStore } from "@netlify/blobs";
 
 export async function handler() {
-  const store = getStore("test-store");
+  const store = getStore({
+    name: "test-store",
+    siteID: process.env.NETLIFY_SITE_ID,
+    token: process.env.NETLIFY_AUTH_TOKEN
+  });
 
   await store.set("hello", { message: "Blobs are working" });
   const value = await store.get("hello");
