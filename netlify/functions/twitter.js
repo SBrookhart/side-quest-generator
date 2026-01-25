8fd4dd350f47a8c4a3b1862d8700a15e9f27ec70
SBrookhart <sabina.brookhart@gmail.com>
2026-01-25 11:45:27 -0500

    Update twitter.js

--- netlify/functions/twitter.js
+++ netlify/functions/twitter.js
@@ -1,22 +1,70 @@
-export async function getTwitterSignals() {
-  const token = process.env.TWITTER_BEARER_TOKEN;
-  if (!token) return [];
+// netlify/functions/twitter.js
 
-  const res = await fetch(
-    "https://api.twitter.com/2/tweets/search/recent?query=someone%20should%20build&max_results=10",
-    {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
+export async function getTwitterSignals() {
+  const token =
+    process.env.TWITTER_BEARER_TOKEN ||
+    process.env.X_BEARER_TOKEN;
+
+  if (!token) {
+    return [];
+  }
+
+  // Strong “someone should build” / unmet-need energy
+  const query = [
+    '"someone should build"',
+    '"wish there was"',
+    '"why isn’t there"',
+    '"missing tool"',
+    '"there should be a"',
+    '"no good way to"'
+  ].join(" OR ");
+
+  const url =
+    "https://api.twitter.com/2/tweets/search/recent?" +
+    new URLSearchParams({
+      query,
+      "tweet.fields": "author_id,created_at,public_metrics",
+      expansions: "author_id",
+      "user.fields": "username",
+      max_results: "10"
+    }).toString();
+
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`
     }
-  );
+  });
+
+  if (!res.ok) {
+    return [];
+  }
 
   const data = await res.json();
+  if (!data.data || !data.includes?.users) {
+    return [];
+  }
 
-  return (data.data || []).map(tweet => ({
-    type: "x",
-    name: "X / Twitter",
-    url: `https://x.com/i/web/status/${tweet.id}`,
-    text: tweet.text
+  const users = Object.fromEntries(
+    data.includes.users.map(u => [u.id, u])
+  );
+
+  return data.data.map(tweet => ({
+    type: "twitter",
+    name: "X Post",
+    text: tweet.text,
+    url: `https://x.com/${users[tweet.author_id]?.username}/status/${tweet.id}`
   }));
+}
+
+// ----- HTTP handler (for manual testing) -----
+export default async function handler() {
+  try {
+    const signals = await getTwitterSignals();
+    return Response.json(signals, { status: 200 });
+  } catch {
+    return Response.json(
+      { error: "Twitter ingestion failed" },
+      { status: 500 }
+    );
+  }
 }
